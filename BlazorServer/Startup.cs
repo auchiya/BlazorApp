898e7aa98c823a3b93bc4631d8d4659b8500643b
Agustin Uchiya <agustinuchiya@chevron.com>
2020-09-10 17:35:39 -0300

    People CRUD in server

--- BlazorServer/Startup.cs
+++ BlazorServer/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorServer.Data;
+using DataAccessLibrary;
 
 namespace BlazorServer
 {
@@ -29,6 +30,8 @@ namespace BlazorServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+            services.AddTransient<IPeopleData, PeopleData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
